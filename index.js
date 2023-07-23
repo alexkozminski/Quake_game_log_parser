7e7365eb5ca8e1e159de0cf69756045500a958c8
Aleksander Kozminski <102160228+alexkozminski@users.noreply.github.com>
2023-07-23 08:16:52 +0200

    parse game log

--- index.js
+++ index.js
@@ -15,10 +15,67 @@ function init () {
     gameId++
 }
 
+function isKill (str) {
+    return str.toLowerCase().indexOf('kill') > -1
+}
+
+function isSeparator (str) {
+    return str.toLowerCase().indexOf('-----') > -1
+}
+
+function getLoser (arr) {
+    return arr.slice(arr.indexOf('killed') + 1, arr.indexOf('by')).join(' ')
+}
+
+function getWinner (arr) {
+    
+    return arr.slice(arr.indexOf('Kill:') + 4, arr.indexOf('killed')).join(' ')
+}
+
 function parseGame(logs) {
 
-    console.log(logs)
+    const logsArr = logs.split("\n")
+    const length = logsArr.length
+    let initiated = false
+    console.log('logs length', logsArr.length)
+    // iterate rows
+    for (let i = 0; i < length; i++) {
+        const row = logsArr[i]
+
+        if (isSeparator(row)) {
+            if (initiated) {
+                const log = {
+                    [`game_${gameId}`]: {
+                        total_kills: totalKills,
+                        players: Array.from(players),
+                        kills,
+                    }
+                }
+                
+                result.push(log)
+                init()
+            }
+            initiated = !initiated
+        } else if (isKill(row)) {
+            totalKills ++;
+            const cols = row.split(' ')
+            const winner = getWinner(cols)
+            const loser = getLoser(cols)
+
+            players.add(winner)
+            players.add(loser)
+
+            if (winner.indexOf('<world>') > -1) {
+                if (kills[loser]) kills[loser]--;
+                else kills[loser] = -1
+            } else {
+                if (kills[winner]) kills[winner] ++
+                else kills[winner] = 1
+            }
+        }
+    }
 }
+
 function main() {
     fs.readFile('./qgames.log', 'utf8', (err, data) => {
         if (err) {
@@ -33,3 +90,5 @@ function main() {
     })
 
 }
+
+main()
